bda604e2030c9628713d7b7f55381b3940a1370a
sangsang6914 <sangsang6914@bandisnc.com>
2022-03-22 09:17:00 +0900

    slack issue Test

--- src/components/board/Study.tsx
+++ src/components/board/Study.tsx
@@ -8,6 +8,7 @@ function Study({type}: IStudyProps) {
   return (
     // Developer, Design ... 등 상위컴포넌트에서 type을 받아서 list api all
     // listitems를 하위 DataTable로 전달
+    // slack issue (commit)
     <div>스터디 모집</div>
   );
 }
